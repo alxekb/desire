6efc189c1b0616e7a8f66cb5cd5ab91a126d2a3f
AlexIv <alx.ekb@gmail.com>
2019-09-04 23:47:52 +0500

    Add token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   validates :login, presence: true, length: { minimum: 3}, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
   validates :signature, :name, presence: true
+
+  before_create :generate_token
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.base64(64)
+  end
 end

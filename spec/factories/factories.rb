99fdf4cf692e5d4009ffa40f21773526a02b6040
AlexIv <alx.ekb@gmail.com>
2019-09-04 23:47:05 +0500

    Add user and post factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,13 +4,13 @@ FactoryBot.define do
     password { 123123123 }
     name { Faker::Name.name }
     signature { Faker::Quote.famous_last_words }
+  end
 
-    factory :post do
-      title {  Faker::Lorem.title }
-      notice { Faker::Lorem.sentence }
-      content { Faker::Lorem.paragraph }
-      published { false }
-      association(:user)
-    end
+  factory :post do
+    title {  Faker::Lorem.sentence }
+    notice { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { true }
+    association(:user)
   end
 end

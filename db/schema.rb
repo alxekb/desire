41b9cd5a1922c0306db608cd173dabf2beccc22b
AlexIv <alx.ekb@gmail.com>
2019-09-06 01:08:32 +0500

    Fix user.password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_101734) do
+ActiveRecord::Schema.define(version: 2019_09_05_170711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,11 +37,13 @@ ActiveRecord::Schema.define(version: 2019_09_04_101734) do
 
   create_table "users", force: :cascade do |t|
     t.string "login"
-    t.string "password"
+    t.string "password_digest"
     t.text "signature"
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token"
   end
 
   add_foreign_key "posts", "users"

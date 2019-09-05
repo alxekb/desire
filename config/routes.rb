7665f1b519f99322c8a5195227868f5fea74b5e9
AlexIv <alx.ekb@gmail.com>
2019-09-06 01:08:57 +0500

    Routes add /login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   scope module: :v1 do
     resources :news, only: [:index, :show, :create, :update]
     resources :authors, only: [:index, :show]
+    post '/login', to: 'authenticate#login'
   end
 end

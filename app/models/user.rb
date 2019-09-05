cff9417b3f759979ab25ed0e20b9fdf8f49a472d
AlexIv <alx.ekb@gmail.com>
2019-09-06 01:12:55 +0500

    Fix user nodel password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  has_many :unreads
-  has_many :posts, through: :unreads
-  validates :login, presence: true, length: { minimum: 3}, uniqueness: true
-  validates :password, presence: true, length: { in: 6..20 }
+  has_many :posts
+  validates :login, presence: true, length: { minimum: 3},
+                    uniqueness: { cast_sensitive: false }
+  validates :password_digest, presence: true
   validates :signature, :name, presence: true
 
   before_create :generate_token
+  has_secure_password
 
   private
 

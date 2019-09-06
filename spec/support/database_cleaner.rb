66c7427fce8a50151fbb77f54a32062924aa9b24
AlexIv <alx.ekb@gmail.com>
2019-09-06 13:40:19 +0500

    Spec support

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,6 +3,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end

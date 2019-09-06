f11e93e748a4bad07ff48081197753c034f5a007
AlexIv <alx.ekb@gmail.com>
2019-09-06 11:13:14 +0500

    Add authenticate controller

---
+++ app/controllers/authenticate_controller.rb
@@ -0,0 +1,15 @@
+class AuthenticateController < ApplicationController
+  skip_before_action :authorize_request, only: :authenticate
+
+  def authenticate
+    auth_token =
+    AuthenticateUser.new(auth_params[:login], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+  def auth_params
+    params.permit(:login, :password)
+  end
+end

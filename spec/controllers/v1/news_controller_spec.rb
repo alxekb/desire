36733f12dde2daf059aec88937431e490fad1d01
AlexIv <alx.ekb@gmail.com>
2019-09-06 11:12:40 +0500

    Add specs and helpers

--- spec/controllers/v1/news_controller_spec.rb
+++ spec/controllers/v1/news_controller_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 
 RSpec.describe V1::NewsController, type: :controller do
   describe '#index' do
-    subject { get :index }
+    let!(:post) { build :post}
+    # subject { get :index }
+
+    it 'response' do
+      get news_index_path
+      json = JSON.parse(response.body)
+      expect(json['users'].count).to eq(1)
+    end
+
     context 'authorize' do
-      it { is_expected.to have_http_status :success }
+      it { is_expected.to     have_http_status :success }
       it { is_expected.not_to be_unauthorized }
     end
+  end
+
+
+  describe '#show' do
+    subject { get :show}
+    context ''
   end
 
   describe '#create' do
     subject { create :news }
 
     context 'as unauthorized' do
-      
+
     end
 
     context 'as authorized' do
@@ -25,19 +39,4 @@ RSpec.describe V1::NewsController, type: :controller do
       end
     end
   end
-
-  describe '#update' do
-
-  end
-
-
-  describe '#author_news' do
-
-  end
-
-  describe '#unread_news' do
-
-  end
-
-
 end

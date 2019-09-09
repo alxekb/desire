53affb75bb205b7b18addcc4e61e677c7832d9e1
AlexIv <alx.ekb@gmail.com>
2019-09-09 17:22:40 +0500

    RSpec request /authors

--- spec/requests/authenticate_spec.rb
+++ spec/requests/authenticate_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe 'Authenticate', type: :request do
+RSpec.describe AuthenticateUser, type: :request do
   describe 'POST /login' do
     let(:user) { create(:user) }
-    let(:headers) { { 'Authorization' => token_generator(user.id) } }
-    let(:invalid_headers) { { 'Authotization' => nil } }
+    let(:headers) { headers_with_token.except('Authorization') }
+    let(:valid_credentials)  do
+      {
+        login: user.login,
+        password: user.password
+      }
+    end
+    let(:invaild_credentials) do
+      {
+        login: 'foo',
+        password: Faker::Internet.password
+      }
+    end
 
-    context 'with walid token' do
-      
+    context 'when request is vaild' do
+      before { post '/login', params: valid_credentials, headers: headers }
+
+      it "returns an user's authentication token" do
+        expect(response).to have_http_status 200
+        expect(json['auth_token']).not_to be_nil
+      end
+    end
+
+    context 'when request is invalid' do
+      before { post '/login', params: invaild_credentials, headers: headers}
+
+      it 'returns an error' do
+        expect(response).to have_http_status 401
+        expect(json['message']).to match(/Invalid credentials./)
+      end
     end
   end
 end
--- spec/requests/author_spec.rb
+++ spec/requests/author_spec.rb
@@ -1,12 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe 'Authors API' do
-  let(:author) { create(:user) }
-  let(:news) { create(:news), user: author }
+  let(:user) { create(:user) }
+  let!(:post) { create(:post, user_id: user.id) }
   let(:headers) { headers_with_token }
-  let(:invalid_headers) { headers_without_token }
+  let(:empty_headers) { headers_with_empty_token }
 
   describe ' GET /authors' do
-    before { get '/autors' }
+    context 'when getting an authors URL' do
+      before { get '/authors', params: {}, headers: headers }
+
+      it 'retutns a status 200' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'returns a JSON array' do
+        expect(json).not_to be_empty
+        expect(json['authors'].count).to eq 1
+      end
+
+      it 'returns authors\'s data' do
+        expect(json['authors'][0]['name']).to eq user.name
+        expect(json['authors'][0]['signature']).to eq user.signature
+      end
+    end
   end
+
+  describe 'GET /authors/:id' do
+    context 'when getting author\s URL'
+      before { get "/authors/#{user.id}", params: user.id, headers: headers_with_empty_token }
+
+      it 'returns a status 200' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'returns an autor\s news' do
+        expect(json['news'].count).to eq 1
+        expect(json['news'][0]['title']).to eq user.posts.last.title
+        expect(json['news'][0]['notice']).to eq user.posts.last.notice
+        expect(json['news'][0]['content']).to eq user.posts.last.content
+      end
+
+      it 'returns an author signature' do
+        expect(json['author']['signature']).to eq user.signature
+      end
+    end
 end

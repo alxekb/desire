ca7b97b50efef8109cd93834e19568ce1ae54747
AlexIv <alx.ekb@gmail.com>
2019-09-09 20:52:51 +0500

    RSpec requests /news

--- spec/requests/news_spec.rb
+++ spec/requests/news_spec.rb
@@ -1,31 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'News API', type: :request do
-  let!(:news) { create(:post) }
-  let(:news_id) { news.id }
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post, user_id: user.id) }
+  let!(:post2) { create(:post, user_id: user.id) }
+  let!(:post3) { create(:post, user_id: user.id) }
+  let(:headers) { headers_with_token }
+  let(:empty_headers) { headers_with_empty_token }
 
-  describe 'GET /news' do
-    before { get '/news' }
+  describe 'GET /news with valid token' do
+    before { get '/news', headers: headers }
 
     it 'return status 200' do
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status 200
     end
 
-    it 'return news' do
+    it 'return valid news' do
       expect(json).not_to be_empty
-      expect(json.size).to eq(1)
+      expect(json['news'].count).to eq 3
+      expect(json['news'][0]['title']).to eq post.title
+      expect(json['news'][1]['notice']).to eq post2.notice
+      expect(json['news'][2]['content']).to eq post3.content
     end
   end
 
-  describe 'GET /news/:id' do
-    before { get "/news/#{news_id}" }
+  describe 'GET /news/:id vith valid token' do
+    before { get "/news/#{post.id}", params: {}, headers: headers }
 
     it 'return status 200' do
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status 200
     end
 
     it 'return news' do
       expect(json).not_to be_empty
+      expect(json['news'].count)
+    end
+
+    before { get '/news', headers: headers }
+
+    it 'excludes news from user\'s feed' do
+        expect(json['news'].count).to eq 2
+        expect(json['news'][0]['title']).not_to eq post.title
+        expect(json['news'][0]['title']).to eq post2.title
     end
   end
 end
